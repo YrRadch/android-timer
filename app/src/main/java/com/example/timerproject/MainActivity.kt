412e913823aefb0be7420a6f1f1fe48e6a2a3017
Gross <tessport777@gmail.com>
2019-11-08 12:42:02 +0200

    12

--- app/src/main/java/com/example/timerproject/MainActivity.kt
+++ app/src/main/java/com/example/timerproject/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.timerproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    val timer = Timer (300000/ 1000)
+        start.setOnClickListener {timer.start ()}
+        stop.setOnClickListener {timer.stop ()}
+
     }
+
+    class Timer(millisInFuture: Long, countDownInterval: Long) : Timer(millisInFuture, countDownInterval)
+
 }
